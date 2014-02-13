1e2e463517a416952c52297a19214b695051663a
llamositopia <napkinorton@gmail.com>
2014-02-12 21:43:33 -0500

    Work

--- Eugine/src/net/llamositopia/eugine/chars/SquishyGenie.java
+++ Eugine/src/net/llamositopia/eugine/chars/SquishyGenie.java
@@ -5,7 +5,7 @@ import net.llamositopia.eugine.Squishy;
 public class SquishyGenie extends Squishy {
     public static SquishyGenie genie = new SquishyGenie();
     public SquishyGenie() {
-        super("genie", 10, 10, 110);
+        super("genie", 10, 5, 110);
     }
 
     public void usePrimaryAbility() {
